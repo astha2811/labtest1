9533eef2e3902ffa1aa34d6cbb0325e7d0e65c4c
astha2811 <144266899+astha2811@users.noreply.github.com>
2024-09-17 16:22:22 +0530

    L1.py

---
+++ q1.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+from scipy import linalg, io, special, sparse
+import numpy as np
+patient_list = np.array([[1,'Astha',2,20,'dengue','complete',416,5000],[2,'Sakshi',1,20,'fever','done',119,800]])
+print(patient_list)
+doctors_list = np.array ([[1,'Abhay','Bdms'],[2,'Neelam','Mbbs'],[3,'Ajay','Mahs']]
+print(doctors_list)
+def find_patients(doctors_id)
+    if patient_list[1,3]==doctor_id:
+        return patient_list[1,2]
+    else
+        return 0
+ doctors_id=1
+print(find_patients(1))
+def updation(patient_list)
+    update[0,5]
+    return patient_list
+    
